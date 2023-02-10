0ce69a93a34e551122698e5af86ef96b4fe360ac
Orel Moshe Hochenboym <orelhochenboym@gmail.com>
2023-02-10 16:52:52 +0200

    feat(types): create types lib and stock-historical-data-year

---
+++ libs/types/src/stock-historical-data-years.ts
@@ -0,0 +1,6 @@
+export class StockHistoricalDataYear {
+  // if a certain stat can be undefined, it should be show as 'N/A' in the client.
+  oneYear: number | undefined;
+  fiveYear: number | undefined;
+  tenTear: number | undefined;
+}
