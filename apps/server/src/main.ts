3c5fdd32d596788e6adeef96f9c2cdaa793a297b
Orel Moshe Hochenboym <orelhochenboym@gmail.com>
2023-02-10 16:45:58 +0200

    feat(swagger): enable and map swagger

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -5,11 +5,22 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule } from '@nestjs/swagger/dist';
 
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Money Meets Value')
+    .setVersion('1.0')
+    .addTag('money-meets-value')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3333;
